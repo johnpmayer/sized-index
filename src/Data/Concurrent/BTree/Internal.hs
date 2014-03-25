d85f5dd7536fd8412e738b3fa6c3423c62a9d0ce
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2014-03-24 20:36:40 -0400

    get rid of STM, not a good idea. emphasis on sizedness

--- src/Data/Concurrent/BTree/Internal.hs
+++ src/Data/Concurrent/BTree/Internal.hs
@@ -16,8 +16,7 @@
 
 module Data.Concurrent.BTree.Internal where
 
-import Control.Applicative 
-import Control.Concurrent.STM 
+import Control.Applicative ()
 import Data.Type.Natural 
 import Data.Vector.Sized (Vector(Nil,(:-)))
 import qualified Data.Vector.Sized as V
@@ -40,18 +39,18 @@ class Keyed k a where
 
 data BNode :: IsRoot -> Height -> Order -> Nat -> * -> * -> * where
   Leaf :: (Keyed k a, NodeInv m n, LeafInv r m) => 
-    TVar (Vector a n) -> 
+    Vector a n -> 
     BNode (R r) (H Zero) (O m) n k a
   Internal :: (Keyed k a, NodeInv m (S n), InternalInv r m (S n)) =>
-    TVar (Vector (forall n'. BNode (R False) (H h) (O m) n' k a) (S n), Vector k n) ->
+    (Vector (forall n'. BNode (R False) (H h) (O m) n' k a) (S n), Vector k n) ->
     BNode (R r) (H (S h)) (O m) (S n) k a
 
-empty :: (Two :<= order, Keyed k a) => STM (BNode (R True) (H Zero) (O order) Z k a)
-empty = Leaf <$> newTVar Nil
+empty :: (Two :<= order, Keyed k a) => BNode (R True) (H Zero) (O order) Z k a
+empty = Leaf Nil
 
 singleton :: (Two :<= order, Keyed k a, {-infer...-} S Z :<= (Two :* order)) => 
-  a -> STM (BNode (R True) (H Zero) (O order) (S Z) k a)
-singleton x = Leaf <$> newTVar (V.singleton x)
+  a -> BNode (R True) (H Zero) (O order) (S Z) k a
+singleton x = Leaf $ V.singleton x
 
 searchLeaf :: (Eq k, Keyed k a) => k -> Vector a n -> [a]
 searchLeaf key vec = filter (\x -> key == tokey x) . V.toList $ vec
@@ -63,9 +62,7 @@ choosePointer :: (Ord k) => k ->
 choosePointer _ (a :- _) Nil = a
 choosePointer key (a :- as) (b :- bs) = if key < b then a else choosePointer key as bs
 
-search :: (Ord k, Keyed k a) => k -> forall n. BNode (R r) (H h) (O m) n k a -> STM [a]
-search key (Leaf leaf) = searchLeaf key <$> readTVar leaf
-search key (Internal node) = do
-  (cpts,keys) <- readTVar node
-  search key $ choosePointer key cpts keys
+search :: (Ord k, Keyed k a) => k -> forall n. BNode (R r) (H h) (O m) n k a -> [a]
+search key (Leaf xs) = filter (\x -> key == tokey x) . V.toList $ xs
+search key (Internal (cpts,keys)) = search key $ choosePointer key cpts keys
 
